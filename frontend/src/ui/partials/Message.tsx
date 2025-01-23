90d38ad98f3b7a74ac7748c325705a32d814d9e2
Otavio Gabriel Alves Barbosa <otaviogabriel.alves.barbosa@gmail.com>
2025-01-23 15:12:13 -0400

    chore: update

--- frontend/src/ui/partials/Message.tsx
+++ frontend/src/ui/partials/Message.tsx
@@ -3,6 +3,8 @@ import {messageInterface} from "../../data/@types/userData";
 import HeaderMessage from "./HeaderMessage";
 import "../styles/Message.css"
 
+
+
 interface MessageProps {
 	listMessages: messageInterface[],
 	toUserState: [number, React.Dispatch<React.SetStateAction<number>>]
@@ -22,28 +24,36 @@ export default function Message({listMessages, toUserState}:MessageProps) {
 			<HeaderMessage setReturn={setToUser}/>
 
 
-			{/* boobbles */}
+			{/* bubbles */}
 			<div className="fixed w-full h-full z-0 overflow-hidden">
-				{/* booble 1 */}
+				{/* bubbles 1 */}
 				<div className="text-black w-96 h-96 rounded-full bg-white absolute right-3/4 -top-1/4">
 				</div>
 
-				{/* booble 2 */}
+				{/* bubbles 2 */}
 				<div className="text-black w-16 h-16 rounded-full bg-red-200 absolute right-1/4 top-2/3">
 				</div>
 
-				{/* booble 3 */}
+				{/* bubbles 3 */}
 				<div className="text-black w-28 h-28 rounded-full bg-green-200 absolute left-1/3 bottom-1/2">
 				</div>
 
-				{/* booble 4 left-2/3 top-1/4   right-72 top-52 */}
+				{/* bubbles 4 left-2/3 top-1/4   right-72 top-52 */}
 				<div className="text-black w-44 h-44 rounded-full bg-gray-200 absolute left-2/3 top-1/4">
 				</div>
 
 
-				{/* booble 4 right-3/4 top-2/3  -left-10 bottom-10*/}
+				{/* bubbles 4 right-3/4 top-2/3  -left-10 bottom-10*/}
+				{/* instalar nova versao taisindcss */}
 				<div className="
-					booble
+					shadow-bubble 
+					animate-floatBubble
+					bg-radial-[at_50%_75%]
+
+					from-fuchsia-600 from-30% 
+					via-indigo-700 via-50% 
+					to-cyan-300
+
 					w-72 
 					h-72 
 					rounded-full 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,14 +5,26 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes:{
+        floatBubble: {
+          '0%, 100%': {
+            transform: 'translateY(0)',
+          },
+          '50%': {
+            transform: 'translateY(-10px)',
+          }
+        }
+      },
       animation: {
         'spin-3s': 'spin 3s linear infinite',
         'spin-4s': 'spin 4s linear infinite',
-        'spin-5s': 'spin 5s linear infinite'
+        'spin-5s': 'spin 5s linear infinite',
+        'floatBubble': 'floatBubble 4s ease-in-out infinite',
       },
-	  boxShadow: {
+	    boxShadow: {
         'outline-sm': '0px 0px 15px -1px #6d28d9',
-		'outline-md': '0px 0px 25px -1px #6d28d9'
+		    'outline-md': '0px 0px 25px -1px #6d28d9',
+        'bubble': 'rgba(0, 0, 0, 0.30) 0px -12px 16px 0px inset,rgba(0, 0, 0, 0.09) 0px -10px 15px -8px',
       }
     },
   },
